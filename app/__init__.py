445ca990bf1ac058f5b3d3cf2f1480863d703b78
Mike Zhong <mzhong@bluebirdbio.com>
2018-01-24 16:23:09 -0500

    1.24.18 ch. 8 complete

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,51 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import os
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
 
 # application is a Flask instance
 application = Flask(__name__)
 application.config.from_object(Config)
+
 # db takes application instance to instantiate
 db = SQLAlchemy(application)
 migrate = Migrate(application, db)
+
 # login manager
 login = LoginManager(application)
 login.login_view = 'login'
 
-from app import routes, models
+# logger - email
+if not application.debug:
+    if application.config['MAIL_SERVER']:
+        auth = None
+        if application.config['MAIL_USERNAME'] or application.config['MAIL_PASSWORD']:
+            auth = (application.config['MAIL_USERNAME'], application.config['MAIL_PASSWORD'])
+        secure = None
+        if application.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(application.config['MAIL_SERVER'], application.config['MAIL_PORT']),
+            fromaddr='no-reply@' + application.config['MAIL_SERVER'],
+            toaddrs=application.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        application.logger.addHandler(mail_handler)
+
+# logger - file
+if not application.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    application.logger.addHandler(file_handler)
+
+    application.logger.setLevel(logging.INFO)
+    application.logger.info('Microblog startup')
+
+from app import routes, models, errors
--- app/models.py
+++ app/models.py
@@ -6,6 +6,11 @@ from app import login
 from hashlib import md5
 
 
+followers = db.Table('followers',
+                     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+                     db.Column('followed_id', db.Integer, db.ForeignKey('user.id')))
+
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
@@ -21,6 +26,14 @@ class User(UserMixin, db.Model):
     about_me = db.Column(db.String(256))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'),
+        lazy='dynamic'
+    )
+
     def __repr__(self):
         return 'User: {}'.format(self.username)
 
@@ -34,6 +47,27 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+            followers.c.follower_id == self.id)
+
+        own = Post.query.filter_by(user_id=self.id)
+
+        return followed.union(own).order_by(Post.timestamp.desc())
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -43,3 +77,4 @@ class Post(db.Model):
 
     def __repr__(self):
         return 'Post: {}'.format(self.body)
+
